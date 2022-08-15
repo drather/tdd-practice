ffed4d70d3106b85fcd34586bc9cccdcc2400d0c
kks <drather@naver.com>
2022-08-15 23:32:14 +0900

    chore: project directory

--- src/test/java/com/example/tddpractice/TddPracticeApplicationTests.java
+++ src/test/java/com/example/tddpractice/TddPracticeApplicationTests.java
@@ -3,11 +3,22 @@ package com.example.tddpractice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TddPracticeApplicationTests {
 
     @Test
     void contextLoads() {
     }
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
 
+        product = five.times(3);
+        assertEquals(15, product.amount);
+
+    }
 }

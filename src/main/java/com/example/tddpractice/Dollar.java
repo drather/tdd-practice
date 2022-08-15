5c8fa6980fb084c89b4e96db74a7c934f24c1c5b
kks <drather@naver.com>
2022-08-15 23:31:42 +0900

    feat: add Dollar class for green test

--- src/main/java/com/example/tddpractice/Dollar.java
+++ src/main/java/com/example/tddpractice/Dollar.java
@@ -5,9 +5,11 @@ public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
-
+        this.amount = amount;
     }
 
-    public void times(int multiplier) {
+    Dollar times(int multiplier) {
+        amount *= multiplier;
+        return new Dollar(amount * multiplier);
     }
 }

3cff42f67601baf29d5ae39661587f97d984e497
kks <drather@naver.com>
2022-08-20 16:07:12 +0900

    ch07 - Dollar, Franc 의 비교 로직 및 테스트 작성

--- src/test/java/com/example/tddpractice/MoneyTest.java
+++ src/test/java/com/example/tddpractice/MoneyTest.java
@@ -11,6 +11,6 @@ class MoneyTest {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
-
 }

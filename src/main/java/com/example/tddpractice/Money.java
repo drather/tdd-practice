74242f87cb25307d8e764866af2688a77c8249bd
kks <drather@naver.com>
2022-08-21 23:10:56 +0900

    ch13: - 통화간 계산을 위한 Expression Interface 추가 - 환율을 위한 Bank 추가

--- src/main/java/com/example/tddpractice/Money.java
+++ src/main/java/com/example/tddpractice/Money.java
@@ -35,6 +35,7 @@ class Money implements Expression {
     }
 
     Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
     }
+
 }
--- src/test/java/com/example/tddpractice/AdditionTest.java
+++ src/test/java/com/example/tddpractice/AdditionTest.java
@@ -3,6 +3,7 @@ package com.example.tddpractice;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AdditionTest {
 
@@ -15,4 +16,22 @@ public class AdditionTest {
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
     }
+
+    @Test
+    public void testPlusReturnsSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
+
+   @Test
+   public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+   }
 }
